28127934fbcaf5385e08099dddc185ceebd64dff
Julian Rojas <julian.rojas87@gmail.com>
2023-08-13 17:23:23 +0200

    Implement feature to deal with both edge- and node-weighted graphs

--- test/NetworkGraph.test.js
+++ test/NetworkGraph.test.js
@@ -9,7 +9,7 @@ import { NetworkGraph } from "../lib/index.js";
  *(1,1)        |        (6,1)        ↑
  *             --------➔ <D> ---------
  *                      (6,0)
- * NODE costs:
+ * NODE weight costs:
  * <A>: 1 
  * <B>: 5
  * <C>: 10
@@ -17,23 +17,31 @@ import { NetworkGraph } from "../lib/index.js";
  * <E>: 4
  */
 
-export function buildTestGraph() {
+export function buildTestGraph(nodeWeighted) {
     const NG = new NetworkGraph();
     // Add nodes costs, coordinates and implicit edges to the graph
-    NG.setNode({ id: "A", nextNode: "B", cost: 1, coordinates: [1, 1] });
+    NG.setNode({ id: "A", nextNode: "B", coordinates: [1, 1] });
     NG.setNode({ id: "A", nextNode: "C" });
-    NG.setNode({ id: "B", nextNode: "C", cost: 5, coordinates: [3, 1] });
+    NG.setNode({ id: "B", nextNode: "C", coordinates: [3, 1] });
     NG.setNode({ id: "B", nextNode: "D" });
-    NG.setNode({ id: "C", nextNode: "E", cost: 10, long: 6 });
+    NG.setNode({ id: "C", nextNode: "E", long: 6 });
     NG.setNode({ id: "C", lat: 1 });
-    NG.setNode({ id: "D", nextNode: "E", cost: 5, coordinates: [6, 0] });
-    NG.setNode({ id: "E", nextNode: "C", cost: 4, coordinates: [9, 1] });
+    NG.setNode({ id: "D", nextNode: "E", coordinates: [6, 0] });
+    NG.setNode({ id: "E", nextNode: "C", coordinates: [9, 1] });
+
+    if (nodeWeighted) {
+        NG.setNode({ id: "A", cost: 1 });
+        NG.setNode({ id: "B", cost: 5 });
+        NG.setNode({ id: "C", cost: 10 });
+        NG.setNode({ id: "D", cost: 5 });
+        NG.setNode({ id: "E", cost: 4 });
+    }
 
     return NG;
 }
 
 test("Build NetworkGraph", async t => {
-    const NG = buildTestGraph();
+    const NG = buildTestGraph(true);
     t.ok(NG.get("A").nextNodes.has("B"));
     t.ok(NG.get("B").prevNodes.has("A"));
     t.equal(NG.get("B").nextNodes.size, 2);
--- test/ShortestPath.test.js
+++ test/ShortestPath.test.js
@@ -3,11 +3,11 @@ import { buildTestGraph } from "./NetworkGraph.test.js";
 import { Dijkstra, AStar, NBAStar } from "../lib/index.js";
 import Utils from "../lib/utils/Utils.js";
 
-const NG = buildTestGraph();
+const NG_node_weighted = buildTestGraph(true);
 
-test("Test Dijkstra rank limit functionality", async t => {
+test("Test Dijkstra rank limit functionality (node-weighted graph)", async t => {
     const planner = new Dijkstra({
-        NG,
+        NG: NG_node_weighted,
         zoom: 10,
         tilesBaseURL: "http://example.org",
         distance: (node) => { return node.cost }
@@ -22,9 +22,9 @@ test("Test Dijkstra rank limit functionality", async t => {
     t.equal(path.map(n => n.id).join(""), "ABD");
 });
 
-test("Test Dijkstra implementation", async t => {
+test("Test Dijkstra implementation (node-weighted graph)", async t => {
     const planner = new Dijkstra({
-        NG,
+        NG: NG_node_weighted,
         zoom: 10,
         tilesBaseURL: "http://example.org",
         distance: (node) => { return node.cost }
@@ -40,9 +40,9 @@ test("Test Dijkstra implementation", async t => {
     t.equal(path.metadata.dijkstraRank, 5);
 });
 
-test("Test A* implementation", async t => {
+test("Test A* implementation (node-weighted graph)", async t => {
     const planner = new AStar({
-        NG,
+        NG: NG_node_weighted,
         zoom: 10,
         tilesBaseURL: "http://example.org",
         distance: (node) => { return node.cost },
@@ -59,12 +59,85 @@ test("Test A* implementation", async t => {
     t.equal(path.metadata.dijkstraRank, 3);
 });
 
-test("Test NBA* implementation", async t => {
+test("Test NBA* implementation (node-weighted graph)", async t => {
     const planner = new NBAStar({
-        NG,
+        NG: NG_node_weighted,
         zoom: 10,
         tilesBaseURL: "http://example.org",
         distance: (node) => { return node.cost },
+        heuristic: Utils.euclideanDistance
+    });
+
+    const path = await planner.findPath(
+        { id: "A", coordinates: [1, 1] },
+        { id: "E", coordinates: [9, 1] }
+    );
+    
+    t.equal(path.path.map(n => n.id).join(""), "ACE");
+});
+
+const NG_edge_weighted = buildTestGraph(false);
+
+test("Test Dijkstra rank limit functionality (edge-weighted graph)", async t => {
+    const planner = new Dijkstra({
+        NG: NG_edge_weighted,
+        zoom: 10,
+        tilesBaseURL: "http://example.org",
+        distance: Utils.euclideanDistance
+    });
+
+    const path = (await planner.findPath(
+        { id: "A", coordinates: [1, 1] },
+        undefined,
+        4
+    )).path;
+    
+    t.equal(path.map(n => n.id).join(""), "ABD");
+});
+
+test("Test Dijkstra implementation (edge-weighted graph)", async t => {
+    const planner = new Dijkstra({
+        NG: NG_edge_weighted,
+        zoom: 10,
+        tilesBaseURL: "http://example.org",
+        distance: Utils.euclideanDistance
+    });
+
+    const path = await planner.findPath(
+        { id: "A", coordinates: [1, 1] },
+        { id: "E", coordinates: [9, 1] }
+    );
+
+    t.equal(path.path.map(n => n.id).join(""), "ACE");
+    // It takes Dijkstra 5 iterations to find the path
+    t.equal(path.metadata.dijkstraRank, 5);
+});
+
+test("Test A* implementation (edge-weighted graph)", async t => {
+    const planner = new AStar({
+        NG: NG_edge_weighted,
+        zoom: 10,
+        tilesBaseURL: "http://example.org",
+        distance: Utils.euclideanDistance,
+        heuristic: Utils.harvesineDistance
+    });
+
+    const path = await planner.findPath(
+        { id: "A", coordinates: [1, 1] },
+        { id: "E", coordinates: [9, 1] }
+    );
+
+    t.equal(path.path.map(n => n.id).join(""), "ACE");
+    // It takes A* 3 iterations to find the path
+    t.equal(path.metadata.dijkstraRank, 3);
+});
+
+test("Test NBA* implementation (edge-weighted graph)", async t => {
+    const planner = new NBAStar({
+        NG: NG_edge_weighted,
+        zoom: 10,
+        tilesBaseURL: "http://example.org",
+        distance: Utils.euclideanDistance,
         heuristic: Utils.euclideanDistance
     });
 
